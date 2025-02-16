cd09be6a1b24407053da022e8206da24514d85ce
AvinandanRoy <abhinandanroylipi@gmail.com>
2025-02-16 12:53:50 +0600

    Digital clock are completed and its updating time per 1000 miliseconds means after 1 seconds

---
+++ Digital_Clocks/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Clock from './Clock';
+import Navbar from './Navbar';
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Clock />
+    </>
+  )
+}
+
+export default App
---
+++ Digital_Clocks/src/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+
+const Navbar =()=>{
+    return(
+        <>
+        <nav>
+            <h1>Avinandan Roy</h1>
+            <div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar 
